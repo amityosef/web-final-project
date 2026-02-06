edfe3a6e429ce9a728f30e4bf56f5763defb98c1
עמית יוסף <amityosef28@gmail.com>
2026-02-06 04:31:01 +0200

    file path

--- server/src/routes/multerRoutes.ts
+++ server/src/routes/multerRoutes.ts
@@ -50,9 +50,10 @@ const upload = multer({ storage });
  *                   description: URL of the uploaded file
  */
 router.post("/", upload.single("file"), (req, res) => {
-    const base = `http://${process.env.DOMAIN_BASE}:${process.env.PORT}/`;
-    const relativePath = `public/uploads/${req.file?.filename}`;
-    res.status(200).send({ url: base + relativePath });
+    const base = `http://${process.env.DOMAIN_BASE}`;
+    const port = process.env.environment === "production" ? "" : `:${process.env.PORT}`;
+    const relativePath = `/public/uploads/${req.file?.filename}`;
+    res.status(200).send({ url: base + port + relativePath });
 });
 
 export default router;
